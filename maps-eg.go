647cb4d8ae306d23664c6ed9bbfaec1a5729d946
Sitharaman Deepak Guptha <vzan2012@gmail.com>
2022-09-11 17:13:55 +0200

    feat: Iterating maps

--- maps-eg.go
+++ maps-eg.go
@@ -4,25 +4,36 @@ import "fmt"
 
 func callColors() {
 	// 1. Way to create a map
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"blue":  "#0000ff",
-	// 	"green": "#00ff00",
-	// 	"grey":  "#cccccc",
-	// 	"black": "#000000",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"blue":  "#0000ff",
+		"green": "#00ff00",
+		"grey":  "#cccccc",
+		"black": "#000000",
+	}
 
 	// 2. Another way to create a map
 	// var colors map[string]string;
 
 	// 3. Using make to create a map
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
 	colors["white"] = "#ffffff"
 	fmt.Println(colors)
 
 	// To Delete a key
-	delete(colors, "white")
+	// delete(colors, "white")
 
-	fmt.Println(colors)
+	// fmt.Println(colors)
+
+	// Iterating a map
+	printMap(colors)
+
+}
+
+// Printing a Map
+func printMap(colors map[string]string) {
+	for color, hex := range colors {
+		fmt.Println(color, "key - value", hex)
+	}
 }
